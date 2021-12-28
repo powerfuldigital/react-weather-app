7ed44ea3c262ae3832a822c7c7e24898284782e3
powerfuldigital <megpower96@hotmail.co.uk>
2021-12-28 17:50:17 +1100

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>Weather App</h1>
+     <a href="https://github.com/powerfuldigital/react-weather-app" target="_blank">Open source code</a>
     </div>
   );
 }
