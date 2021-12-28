533d63bfa9157de01ae292df8ce1a6fc374ab6e2
powerfuldigital <megpower96@hotmail.co.uk>
2021-12-29 06:26:27 +1100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>Weather App</h1>
-     <a href="https://github.com/powerfuldigital/react-weather-app" target="_blank">Open source code</a>
+     <a href="https://github.com/powerfuldigital/react-weather-app" target="_blank" rel="noreferrer">Open source code</a>
+     </div>
     </div>
   );
 }
